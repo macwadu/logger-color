cdc3151fb56ff02ae85cde6feac93dc51925d0ba
Tiago Lourenço Nobre <tiago.nobre@mendeley.com>
2013-05-08 12:12:37 +0100

    change structure

---
+++ tests/tests.rb
@@ -0,0 +1,19 @@
+require '../lib/logger_color'
+
+class Tests
+
+
+
+	 
+def self.t
+
+	  LOG.info("Test")
+	  LOG.warn("Test")
+	  LOG.debug("Test")
+	  LOG.unknown("Test")
+end
+
+
+end
+
+Tests.t
